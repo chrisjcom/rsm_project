bd3d9fc48ecdb32c28f1e1c9002e1ca47eacb652
Jhossymar Contreras <chrisj_contreras@outlook.com>
2022-07-18 21:08:47 -0600

    fix: file local server has an error with static

--- app.js
+++ app.js
@@ -6,10 +6,10 @@ app.get('/', function(req, res) {
     res.sendFile(__dirname + '/dev/index.html');
 })
 
-app.use('/css', express.static(path.join(__dirname + 'dev/css')));
-app.use('/js', express.static(path.join(__dirname + 'dev/js')));
-app.use('/assets', express.static(path.join(__dirname + 'dev/assets')));
-app.use('/vendor', express.static(path.join(__dirname + 'dev/vendor')));
+app.use('/css', express.static(path.join(__dirname, 'dev/css')));
+app.use('/js', express.static(path.join(__dirname, 'dev/js')));
+app.use('/assets', express.static(path.join(__dirname, 'dev/assets')));
+app.use('/vendor', express.static(path.join(__dirname, 'dev/vendor')));
 
 app.listen(3000, function() {
     console.log('Server is running on http://localhost:3000');
